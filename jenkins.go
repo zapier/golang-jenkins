90122e8a97bb300acf3024ed4b3b73410b894ccd
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-01-15 14:39:08 +0100

    support hierarchical item groups

--- jenkins.go
+++ jenkins.go
@@ -359,18 +359,18 @@ func (jenkins *Jenkins) GetJobs() ([]Job, error) {
 
 // GetJob returns a job which has specified name.
 func (jenkins *Jenkins) GetJob(name string) (job Job, err error) {
-	err = jenkins.get(fmt.Sprintf("/job/%s", name), nil, &job)
+	err = jenkins.get(FullPath(name), nil, &job)
 	return
 }
 
 // GetJobUrl returns the URL path for the job with the specified name.
 func (jenkins *Jenkins) GetJobURLPath(name string) string {
-	return fmt.Sprintf("/job/%s", name)
+	return FullPath(name)
 }
 
 //GetJobConfig returns a maven job, has the one used to create Maven job
 func (jenkins *Jenkins) GetJobConfig(name string) (job JobItem, err error) {
-	err = jenkins.getConfigXml(fmt.Sprintf("/job/%s/config.xml", name), nil, &job)
+	err = jenkins.getConfigXml(FullPath(name) + "/config.xml", nil, &job)
 	return
 }
 
@@ -410,8 +410,8 @@ func FullJobPath(path ...string) string {
 }
 
 // FullJobPath returns the full job path URL for the given paths
-func FullPath(job Job) string {
-	paths := strings.Split(job.FullName, "/")
+func FullPath(job string) string {
+	paths := strings.Split(job, "/")
 	return FullJobPath(paths...)
 }
 
